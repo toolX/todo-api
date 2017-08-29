e38ac26efdfbd6ceb933359c28a34fd62937bdd2
Philip Shamsiev <ph.shamsiev@mail.ru>
2017-08-29 14:01:48 +0300

    Configure mlab for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+let db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://toolX:!QAZ2wsx@ds153853.mlab.com:53853/db-for-node-todo-app'
+};
+mongoose.connect(process.env.PORT ? db.mlab : db.localhost);
 
 module.exports = {mongoose};
